3d5e271f69d2c53e693ce01d90e93823c42b298c
victorcomc <victorgabriele@hotmail.com>
2025-11-12 08:57:21 -0300

    tirei o theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 // import './index.css' // <-- Pode remover ou comentar o CSS global antigo se ele estiver atrapalhando
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import theme from './theme'; // <--- Importe o tema que criamos
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
